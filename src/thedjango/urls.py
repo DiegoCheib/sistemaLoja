4ad06a79b4f8038258b6fffe72fc5de7aeb6f9b5
roberto-fernandino <romfernandino@gmail.com>
2023-07-25 20:20:07 -0300

    Created sass files, improved base.html, created copy buttons (not ready yet),

--- src/thedjango/urls.py
+++ src/thedjango/urls.py
@@ -16,10 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import login_view
+from users.views import login_view, signup1_view
+
+
+'''
+robs tutoriais!
+
+Aqui nesse arquivo a gente adicona um import olha aqui em cima e voce vai entender a logica.
+exemplo: linha 19 -> from users(pasta do app users).views(arquivo onde criamos a funcao da view/tela) import Xview,Yview...
+
+'''
+# Aqui declaramos urlpatterns que eh uma lista que vai receber funcoes path onde criaremos um url 
+# melhor sem .html no fim de cada pagina
+# e em seguida no formato (str(url), funcao(tela), facilitarProsAmiguinhos(nome))
+# eh isso...
+# recomendo voce ler o src/thedjango/settings.py la tem mt info E LE A PORRA DA DOCUMENTACAO DO DJANGO DESGRACA
 
 
 urlpatterns = [
-    path('login/', login_view),
-    path('admin/', admin.site.urls),
+    path('signup/', signup1_view, name="cadastro"),
+    path('login/', login_view, name="LoginScreen"),
+    path('admin/', admin.site.urls, name="administration"),
 ]
