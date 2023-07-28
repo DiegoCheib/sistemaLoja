ac3a50afae4744271cb1cd39253534bff24cd9e9
roberto-fernandino <romfernandino@gmail.com>
2023-07-28 12:51:59 -0300

    Atualizado models com data atual e removido data de nascimento para mais pra frente

--- src/cadastro/models.py
+++ src/cadastro/models.py
@@ -2,9 +2,20 @@ from django.db import models
 
 # Create your models here.
 class userTest(models.Model):
-    user_id = models.AutoField(primary_key=True)
+    user_id = models.AutoField(primary_key=True, auto_created=True,)
     username = models.CharField(max_length=25)
-    usersobrenome = models.CharField(max_length=25, default="Sobrenome")
+    usersobrenome = models.CharField(max_length=25)
+    useremail = models.CharField(max_length=100, unique=True)
+    userpassword = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=17)
+    cpf = models.CharField(max_length=11, unique=True)
+    datacriacao = models.DateTimeField(auto_now_add=True)
+    
+
 
     def __str__(self):
         return f"{self.username}, {self.usersobrenome}"
+
+
+#pra depois    
+#data_nascimento = models.DateField()
